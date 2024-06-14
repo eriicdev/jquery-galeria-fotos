c4e36a2c8cf8e0efab5575ee6618cd3c4dcebe7b
Erik costa <ec5489976@gmail.com>
2024-06-14 08:40:57 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit' , function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="image-link">
+                <a href="${enderecoNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+
+        $('#endereco-imagem-nova').val(' ');
+    })
+})
+
